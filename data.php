d43350304b2964b289ddbbbece3e108307f23a08
Ann Edvinsson <aslin.ann@gmail.com>
2017-10-26 16:25:43 +0200

    commit

--- data.php
+++ data.php
@@ -8,6 +8,7 @@ $posts = [
     'title' => 'title1',
     'content' => 'Lorem Ipsum este pur şi simplu o machetă pentru text a industriei tipografice. Lorem Ipsum a fost macheta standard a industriei încă din secolul al XVI-lea, când un tipograf anonim a luat o planşetă de litere şi le-a amestecat pentru a c',
     'published_date' => '2017-10-11',
+    'image' =>'face1.jpeg',
     'likes' => 9
   ],
 
@@ -17,6 +18,7 @@ $posts = [
     'title' => 'title2',
     'content' => 'll 45 î.e.n., făcând-o să aibă mai bine de 2000 ani. Profesorul universitar de latină de la colegiul Hampden-S',
     'published_date' => '2017-10-11',
+      'image' =>'face1.jpeg',
     'likes' => 10
   ],
 
@@ -24,8 +26,9 @@ $posts = [
     'id' => 2,
     'author' => 'author2',
     'title' => 'title3',
-    'content' => 'coperit la modul cel mai sigur su',
+    'content' => 'coperit la modul cel mai sigur su reţi să e de 2000 ani. Profesorul univers e de 2000 ani. Profesorul univers folosiţi un pasaj de Lorem Ipsum, trebuie săe de 2000 ani. Profesorul univers',
     'published_date' => '2017-10-12',
+      'image' =>'face2.jpeg',
     'likes' => 25
   ],
 
@@ -35,6 +38,7 @@ $posts = [
     'title' => 'title4',
     'content' => 'l tipografia electronică practic neschimbată. A fost popularizată în anii 60 odată cu ieşirea colilor',
     'published_date' => '2017-10-13',
+      'image' =>'face2.jpeg',
     'likes' => 18
   ],
 
@@ -42,8 +46,9 @@ $posts = [
     'id' => 3,
     'author' => 'author3',
     'title' => 'title5',
-    'content' => 'i, conţinut acolo", făcându-l să ',
+    'content' => 'i, conţinut acolo", făcându-l să  A fost popularizată e de 2000 ani. Profesorul univers în anii 60 odată cu e de e de 2000 ani. Profesorul univers 2000 ani. Profesorul univers',
     'published_date' => '2017-10-15',
+      'image' =>'face3.jpg',
     'likes' => 20
   ],
 
@@ -53,6 +58,7 @@ $posts = [
     'title' => 'title6',
     'content' => ' de puţin credibile. Daca vreţi să folosiţi un pasaj de Lorem Ipsum, trebuie să vă asiguraţi că',
     'published_date' => '2017-10-17',
+    'image' =>'face3.jpg',
     'likes' => 17
   ],
 
@@ -62,6 +68,7 @@ $posts = [
     'title' => 'title7',
     'content' => 'l utilizează un dicţionar de peste',
     'published_date' => '2017-10-24',
+      'image' =>'face4.jpg',
     'likes' => 15
   ],
 
@@ -71,6 +78,7 @@ $posts = [
     'title' => 'title8',
     'content' => 'ai jos pentru cei interesaţi. Secţiunile 1.10.32 şi 1.10.33 din "de Finibus Bonorum et Malorum',
     'published_date' => '2017-10-30',
+      'image' =>'face4.jpg',
     'likes' => 10
   ],
 
@@ -80,6 +88,7 @@ $posts = [
     'title' => 'title9',
     'content' => 'liguraţi că nu conţine nimic stânjenitor ascuns printre randuri. Toate generatoarele de Lorem Ipsum de pe Internet tind să repete bucăţi de text în',
     'published_date' => '2017-10-03',
+      'image' =>'face5.jpg',
     'likes' => 4
   ],
 
@@ -89,6 +98,7 @@ $posts = [
     'title' => 'title10',
     'content' => ' Ipsum-ul generat ',
     'published_date' => '2017-10-02',
+      'image' =>'face5.jpg',
     'likes' => 8
 
   ]
