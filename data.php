dd80bf46ebac7f6c11427198fdb283ef63d96710
Ann Edvinsson <aslin.ann@gmail.com>
2017-10-26 22:33:25 +0200

    small adjustments

--- data.php
+++ data.php
@@ -8,7 +8,7 @@ $posts = [
     'title' => 'Drug to Reverse Snus Overdose',
     'content' => 'Progress shared vocabulary, preliminary thinking, do-gooder, shared unit of analysis then academic natural resources. Move the needle LGBTQ+ systems thinking storytelling best practices granular equal opportunity catalyze. Accessibility; deep dive fairness we must stand up peaceful circular activate problem-solvers inspirational. Design thinking shared unit of analysis a, change-makers green space citizen-centered low-hanging fruit program areas. Progress, black lives matter scalable co-creation resilient; progress society correlation. Support, silo a, milestones change-makers, engaging social entrepreneurship. Impact, a equal opportunity, catalyze; greenwashing blended value innovate. Justice justice; indicators, think tank inclusion, collaborative consumption LGBTQ+.',
     'published_date' => '2017-10-11',
-    'image' =>'face1.jpeg',
+    'image' =>'img/face1.jpeg',
     'likes' => 9
   ],
 
@@ -17,7 +17,7 @@ $posts = [
     'title' => 'People start talking about mental illness among cats',
     'content' => 'Impact move the needle or engaging thought leadership; natural resources communities to society. Black lives matter, compassion living a fully ethical life program area families state of play shared vocabulary. Families social capital innovation overcome injustice blended value compelling. Families, rubric resilient; thought leadership greenwashing, social enterprise humanitarian B-corp. Problem-solvers resist overcome injustice inclusive, granular support, improve the world resilient. Social innovation ecosystem the resistance changemaker strengthening infrastructure. Academic philanthropy move the needle, philanthropy incubator humanitarian we must stand up data grit. Effective altruism engaging, empower communities, incubator; compelling revolutionary deep dive or resist.',
     'published_date' => '2017-03-11',
-      'image' =>'face1.jpeg',
+      'image' =>'img/face1.jpeg',
     'likes' => 10
   ],
 
@@ -27,7 +27,7 @@ $posts = [
     'title' => 'Donald Trump Resigns',
     'content' => 'Unprecedented challenge engaging catalyze ideate, equal opportunity support living a fully ethical life. Game-changer empathetic; catalyze because; rubric; or living a fully ethical life capacity building. Efficient corporate social responsibility optimism emerging, think tank a resilient best practices. Incubator corporate social responsibility ideate revolutionary, triple bottom line emerging silo thought provoking disrupt. Expose the truth strategy, impact social enterprise transparent gender rights. Milestones the resistance improve the world movements B-corp social entrepreneur save the world storytelling. Overcome injustice, initiative issue outcomes effective, effective; dynamic our work social entrepreneurship collaborative consumption.',
     'published_date' => '2016-11-12',
-      'image' =>'face2.jpeg',
+      'image' =>'img/face2.jpeg',
     'likes' => 25
   ],
 
@@ -37,7 +37,7 @@ $posts = [
     'title' => 'Culture (CDP) replaces GDP',
     'content' => 'Collaborative cities efficient data, scalable greenwashing to. Her body her rights equal opportunity, collective impact greenwashing inclusion vibrant B-corp best practices global. LGBTQ+, living a fully ethical life empathetic collaborative cities catalyze. Humanitarian inclusive granular; B-corp families, systems thinking inclusion. The resistance; when cultivate progress LGBTQ+ NGO move the needle. Program areas, program areas thought leader sustainable thought partnership move the needle. Innovate, empathetic, framework corporate social responsibility strengthening infrastructure. Black lives matter green space.',
     'published_date' => '2017-10-26',
-      'image' =>'face2.jpeg',
+      'image' =>'img/face2.jpeg',
     'likes' => 18
   ],
 
@@ -47,7 +47,7 @@ $posts = [
     'title' => 'Programming - now compulsory in Swedish elementary schools',
     'content' => 'Humanitarian natural resources accessibility, impact communities commitment. Agile incubator, scale and impact, support, sustainable impact investing synergy leverage synergy. Green space resist social capital relief social innovation uplift. Save the world, social innovation social innovation invest social intrapreneurship corporate social responsibility rubric. The replicable families humanitarian society social impact. Because issue outcomes; inclusion social intrapreneurship catalyze bandwidth theory of change because. Outcomes triple bottom line effective altruism state of play challenges and opportunities. Her body her rights agile thought leadership overcome injustice NGO big data mass incarceration. Social innovation framework, to; progress, collective impact replicable strengthening infrastructure.',
     'published_date' => '2017-10-15',
-      'image' =>'face3.jpg',
+      'image' =>'img/face3.jpg',
     'likes' => 20
   ],
 
@@ -57,7 +57,7 @@ $posts = [
     'title' => 'Matriarchy Found on Mars',
     'content' => 'Storytelling shared value her body her rights social intrapreneurship compelling do-gooder. Innovate shared vocabulary impact; philanthropy framework. Resist, resilient inclusion collective impact, empower mobilize efficient. Game-changer accessibility philanthropy empower, humanitarian shine parse. Her body her rights progress justice improve the world the resistance. Shared vocabulary collaborate commitment inspiring rubric; entrepreneur move the needle. Movements, living a fully ethical life game-changer targeted collaborative cities and the resistance social return on invest.',
     'published_date' => '2017-01-17',
-    'image' =>'face3.jpg',
+    'image' =>'img/face3.jpg',
     'likes' => 17
   ],
 
@@ -66,7 +66,7 @@ $posts = [
     'title' => 'Masculinity is finally transforming',
     'content' => 'Shared unit of analysis capacity building energize entrepreneur B-corp citizen-centered transparent social impact. Preliminary thinking corporate social responsibility strengthening infrastructure her body her rights progress empathetic black lives matter co-creation. Commitment social innovation, her body her rights humanitarian contextualize youth. Think tank change-makers, global; problem-solvers accessibility efficient; and deep dive. Move the needle, impact, benefit corporation compelling thought provoking game-changer. Mass incarceration because thought leader, inspiring commitment; shine revolutionary shared unit of analysis.',
     'published_date' => '2017-10-24',
-      'image' =>'face4.jpg',
+      'image' =>'img/face4.jpg',
     'likes' => 15
   ],
 
@@ -75,16 +75,16 @@ $posts = [
     'title' => 'White Supremacy is Losing Members Radically',
     'content' => 'Energize inclusive then; philanthropy social intrapreneurship when. Social intrapreneurship efficient parse social capital correlation. Blended value thought partnership; youth a initiative. Black lives matter external partners accessibility her body her rights boots on the ground. Effective effective the fairness when, we must stand up corporate social responsibility engaging green space. Fairness social entrepreneurship because empower communities global co-creation vibrant grit. Parse benefit corporation, move the needle because co-create contextualize capacity building. When her body her rights shared vocabulary incubator compelling. Empower communities thought partnership, green space families families bandwidth. Move the needle, white paper B-corp; living a fully ethical life overcome injustice or optimism inclusion. Co-create, triple bottom line targeted incubator co-create.',
     'published_date' => '2017-03-30',
-      'image' =>'face4.jpg',
+      'image' =>'img/face4.jpg',
     'likes' => 10
   ],
 
   [
     'author' => ['name'=> 'Abbey Stockson', 'id' => 5],
     'title' => 'The sexism is Dead',
-    'content' => 'lContextualize movements, living a fully ethical life game-changer targeted collaborative cities and the resistance social return on investment. Black lives matter activate then empower justice compassion unprecedented challenge. Empathetic mobilize, paradigm justice, targeted. Incubator, efficient disrupt greenwashing paradigm. Thought partnership because, compassion deep dive data; social innovation program area impact investing. Capacity building; paradigm big data, compassion efficient benefit corporation outcomes.',
+    'content' => 'Contextualize movements, living a fully ethical life game-changer targeted collaborative cities and the resistance social return on investment. Black lives matter activate then empower justice compassion unprecedented challenge. Empathetic mobilize, paradigm justice, targeted. Incubator, efficient disrupt greenwashing paradigm. Thought partnership because, compassion deep dive data; social innovation program area impact investing. Capacity building; paradigm big data, compassion efficient benefit corporation outcomes.',
     'published_date' => '2016-10-03',
-      'image' =>'face5.jpg',
+      'image' =>'img/face5.jpg',
     'likes' => 4
   ],
 
@@ -93,7 +93,7 @@ $posts = [
     'title' => 'Refugees are now Welcome to Europe',
     'content' => 'Optimism, change-makers, program areas improve the world best practices and greenwashing. Social innovation improve the world low-hanging fruit transparent accessibility agile progress. Activate, green space fairness shared vocabulary leverage. Changemaker, parse innovation families a, we must stand up. Inclusive families; philanthropy but human-centered; peaceful dynamic, vibrant. Replicable empower communities overcome injustice empower communities strategy resilient strategize systems thinking. Overcome injustice issue outcomes, the energize expose the truth improve the world because incubator.',
     'published_date' => '2017-07-02',
-      'image' =>'face5.jpg',
+      'image' =>'img/face5.jpg',
     'likes' => 8
 
   ]
